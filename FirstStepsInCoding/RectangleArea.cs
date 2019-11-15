aa76443b016b3bc56664c5118ea60f3eb0abe0b6
petyaminkova <petyaminkova91@abv.bg>
2019-11-15 21:50:33 +0200

    First steps in coding

---
+++ FirstStepsInCoding/RectangleArea.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AreaOfRectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine(a*b);
+        }
+    }
+}
