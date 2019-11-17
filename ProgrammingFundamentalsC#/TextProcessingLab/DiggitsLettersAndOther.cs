f25ea4cbfeb431714739b328abbb696f471ba4cc
petyaminkova <petyaminkova91@abv.bg>
2019-11-17 22:54:57 +0200

    Text Processing Lab

---
+++ ProgrammingFundamentalsC#/TextProcessingLab/DiggitsLettersAndOther.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _05_DigitsLettersAndOther
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string text = Console.ReadLine();
+            char[] charArray = text.ToCharArray();
+
+            string numbers = "";
+            string letters = "";
+            string symbols = "";
+
+            foreach (var letter in charArray)
+            {
+                if (Char.IsDigit(letter))
+                {
+                    numbers += letter;
+                }
+                else if (Char.IsLetter(letter))
+                {
+                    letters += letter;
+                }
+                else
+                {
+                    symbols += letter;
+                }
+            }
+
+            Console.WriteLine(numbers);
+            Console.WriteLine(letters);
+            Console.WriteLine(symbols);
+
+        }
+    }
+}
---
+++ ProgrammingFundamentalsC#/TextProcessingLab/RepeatString.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace _02_RepeatString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = Console.ReadLine().Split();
+
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                string currentWord = array[i];
+
+                for (int j = 0; j < currentWord.Length; j++)
+                {
+                    result.Append(currentWord);
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
