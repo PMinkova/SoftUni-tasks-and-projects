cd64cc41300c99a55fcff00d916c99c37444dfe7
petyaminkova <petyaminkova91@abv.bg>
2019-11-17 22:59:19 +0200

    Create MatchFullName.cs

---
+++ ProgrammingFundamentalsC#/RegularExpressionsLab/MatchFullName.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01_MatchFullName
+{
+    class MatchFullName
+    {
+        static void Main(string[] args)
+        {
+            var regex = new Regex(@"\b[A-Z][a-z]+ [A-Z][a-z]+\b");
+
+            var text = "Ivan Ivanov, Ivan ivanov, ivan Ivanov, IVan Ivanov, Test Testov, Ivan  Ivanov";
+
+            var matches = regex.Matches(text);
+
+            foreach (Match match in matches)
+            {
+                Console.Write(match.Value + " ");
+            }
+        }
+    }
+}
