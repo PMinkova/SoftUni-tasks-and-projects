685cbcc652464d1a9829080391eb80c1012b04fa
petyaminkova <petyaminkova91@abv.bg>
2019-11-18 14:12:45 +0200

    Regular Expressions Lab

---
+++ ProgrammingFundamentalsC#/RegularExpressionsLab/MatchDates.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03_MatchDates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var regex = new Regex(@"\b(?<day>\d{2})(\/| -|.)(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})\b");
+
+            var text = Console.ReadLine();
+
+            var dates = regex.Matches(text);
+
+            foreach (Match date in dates)
+            {
+                var day = date.Groups["day"].Value;
+                var month = date.Groups["month"].Value;
+                var year = date.Groups["year"].Value;
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+        }
+    }
+}
---
+++ ProgrammingFundamentalsC#/RegularExpressionsLab/MatchPhoneNumber.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02_MatchPnoneNumber
+{
+    class MatchPhoneNumber
+    {
+        static void Main(string[] args)
+        {
+            var regex = new Regex(@"\+359(-| )2\1\d{3}\1\d{4}\b");
+
+            var text = Console.ReadLine();
+
+            var matches = regex.Matches(text);
+
+            Console.WriteLine(String.Join(", ", matches));
+        }
+    }
+}
