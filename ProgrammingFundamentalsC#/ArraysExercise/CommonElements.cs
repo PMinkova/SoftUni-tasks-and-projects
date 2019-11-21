0a4f88f652db7018d6539d24608c995f15493a57
petyaminkova <petyaminkova91@abv.bg>
2019-11-21 22:22:31 +0200

    Arrays Exercise

---
+++ ProgrammingFundamentalsC#/ArraysExercise/CommonElements.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _02_CommonElements
+{
+    class CommonElements
+    {
+        static void Main(string[] args)
+        {
+            string[] firstArray = Console.ReadLine().Split();
+            string[] secondArray = Console.ReadLine().Split();
+
+            for (int i = 0; i < secondArray.Length; i++)
+            {
+                if (firstArray.Contains(secondArray[i]))
+                {
+                    Console.Write(secondArray[i] + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ ProgrammingFundamentalsC#/ArraysExercise/ZigZagArrays.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _03_ZigZagArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] firstArr = new int[n];
+            int[] secArr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] numbers = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+
+                if (i % 2 == 0)
+                {
+                    firstArr[i] = numbers[0];
+                    secArr[i] = numbers[1];
+                }
+                else
+                {
+                    firstArr[i] = numbers[1];
+                    secArr[i] = numbers[0];
+                }
+            }
+            Console.WriteLine(String.Join(" ", firstArr));
+            Console.WriteLine(String.Join(" ", secArr));
+        }
+    }
+}
