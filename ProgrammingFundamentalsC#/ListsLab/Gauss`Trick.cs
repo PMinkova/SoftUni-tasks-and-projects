ebd0e3b38652363f6178bcc6525b50f29a77a111
petyaminkova <petyaminkova91@abv.bg>
2019-11-22 22:43:22 +0200

    Lists Lab

---
+++ ProgrammingFundamentalsC#/ListsLab/Gauss`Trick.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _02_Gauss__Trick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            int size = numbers.Count / 2;
+
+            for (int i = 0; i < size; i++)
+            {
+                numbers[i] += numbers[numbers.Count - 1];
+
+                numbers.RemoveAt(numbers.Count - 1);
+            }
+
+            Console.WriteLine(String.Join(" ", numbers));
+
+        }
+    }
+}
---
+++ ProgrammingFundamentalsC#/ListsLab/MerginLists.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03_MergingLists
+{
+    class MerginLists
+    {
+        static void Main(string[] args)
+        {
+            List<string> firstList = Console.ReadLine().Split().ToList();
+            List<string> secondList = Console.ReadLine().Split().ToList();
+
+            List<string> result = new List<string>();
+
+            int count = Math.Min(firstList.Count, secondList.Count);
+
+            for (int i = 0; i < count; i++)
+            {
+                result.Add(firstList[i]);
+                result.Add(secondList[i]);
+            }
+
+            if (firstList.Count > secondList.Count)
+            {
+                for (int i = count; i < firstList.Count; i++)
+                {
+                    result.Add(firstList[i]);
+                }
+            }
+            else
+            {
+                for (int i = count; i < secondList.Count; i++)
+                {
+                    result.Add(secondList[i]);
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", result));
+        }
+    }
+}
