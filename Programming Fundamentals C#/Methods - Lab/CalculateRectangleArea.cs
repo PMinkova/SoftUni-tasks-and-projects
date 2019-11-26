228c4bc972cb58ea3e8c96d8e25daf40549d5ebc
petyaminkova <petyaminkova91@abv.bg>
2019-11-26 22:35:55 +0200

    Updated repository with tasks

---
+++ Programming Fundamentals C#/Methods - Lab/CalculateRectangleArea.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _06_CalculateRectangleArea
+{
+    class CalculateRectangleArea
+    {
+        static double GetRectangleArea(double width, double height)
+        {
+            return width * height;
+        }
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            double area = GetRectangleArea(width, height);
+
+            Console.WriteLine(area);
+        }
+    }
+}
---
+++ Programming Fundamentals C#/Methods - Lab/Grades.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _02_Grades
+{
+    class Program
+    {
+        static void Main()
+        {
+            double grade = double.Parse(Console.ReadLine());
+
+            PrintGradeAsAWord(grade);
+        }
+
+        static void PrintGradeAsAWord(double grade)
+        {
+            string result = String.Empty;
+
+            if (2.00 <= grade && grade < 3.00)
+            {
+                result = "Fail";
+            }
+            else if (3.00 <= grade && grade < 3.50)
+            {
+                result = "Poor";
+            }
+            else if (3.50 <= grade && grade < 4.50)
+            {
+                result = "Good";
+            }
+            else if (4.50 <= grade && grade < 5.50)
+            {
+                result = "Very good";
+            }
+            else if (5.50 <= grade && grade <= 6.00)
+            {
+                result = "Excellent";
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
