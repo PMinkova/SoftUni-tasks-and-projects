be1bd89cdcaa121a19a96e648ff5c80c8c17e481
petyaminkova <petyaminkova91@abv.bg>
2019-11-28 16:29:31 +0200

    Associative Arrays - Laboratory

---
+++ Programming Fundamentals C#/AssociativeArrays - Exercise/AMinerTask.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02_AMinerTask
+{
+    class AMinerTask
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var resources = new Dictionary<string, int>();
+
+            while (input != "stop")
+            {
+                string resource = input;
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (!resources.ContainsKey(resource))
+                {
+                    resources[resource] = 0;
+                }
+
+                resources[resource] += quantity;
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var item in resources)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
