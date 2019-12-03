e29b4c9cb90900260553ce619316538be058c071
petyaminkova <petyaminkova91@abv.bg>
2019-12-03 14:40:30 +0200

    Exam preparation

---
+++ Programming Fundamentals C#/Final Exam - 03 August 2019 Group 1/MessageMenager.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03_MessageMenager
+{
+    class UserReport
+    {
+        public UserReport(int sent, int received)
+        {
+            Sent = sent;
+            Received = received;
+        }
+        public int Sent { get; set; }
+
+        public int Received { get; set; }
+
+        public int Sum => this.Sent + this.Received;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, UserReport> users = new Dictionary<string, UserReport>();
+
+            int capacity = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+
+            while (input != "Statistics")
+            {
+                string[] commandArgs = input.Split("=");
+                string command = commandArgs[0];
+
+                if (command == "Add")
+                {
+                    string username = commandArgs[1];
+                    int sentMessages = int.Parse(commandArgs[2]);
+                    int receivedMessages = int.Parse(commandArgs[3]);
+
+                    if (!users.ContainsKey(username))
+                    {
+                        users.Add(username, new UserReport(sentMessages, receivedMessages));
+                    }
+
+                }
+                else if (command == "Message")
+                {
+                    string sender = commandArgs[1];
+                    string receiver = commandArgs[2];
+
+                    if (users.ContainsKey(sender) && users.ContainsKey(receiver))
+                    {
+                        users[sender].Sent++;
+
+                        if (users[sender].Sum >= capacity)
+                        {
+                            users.Remove(sender);
+                            Console.WriteLine($"{sender} reached the capacity!");
+                        }
+
+                        users[receiver].Received++;
+
+                        if (users[receiver].Sum >= capacity)
+                        {
+                            Console.WriteLine($"{receiver} reached the capacity!");
+                            users.Remove(receiver);
+                        }
+                    }
+                }
+                else if (command == "Empty")
+                {
+                    string username = commandArgs[1];
+
+                    if (username == "All")
+                    {
+                        users.Clear();
+                    }
+                    else
+                    {
+                        users.Remove(username);
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Users count: {users.Count}");
+
+            foreach (var user in users.OrderByDescending(x => x.Value.Received).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{user.Key} - {user.Value.Sum}");
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals C#/Final Exam - 03 August 2019 Group 1/StringManipulator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualBasic;
+
+namespace _01_StringManipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string commandLine = Console.ReadLine();
+
+            while (commandLine != "End")
+            {
+                string[] commandArgs = commandLine.Split();
+                string command = commandArgs[0];
+
+
+                if (command == "Translate")
+                {
+                    string charToreplace = commandArgs[1];
+                    string replacement = commandArgs[2];
+
+                    input = input.Replace(charToreplace, replacement);
+
+                    Console.WriteLine(input);
+                }
+                else if (command == "Includes")
+                {
+                    string stringToCheck = commandArgs[1];
+                    Console.WriteLine(input.Contains(stringToCheck));
+                }
+                else if (command == "Start")
+                {
+                    string stringToCheck = commandArgs[1];
+                    Console.WriteLine(input.StartsWith(stringToCheck));
+                }
+                else if (command == "Lowercase")
+                {
+                    input = input.ToLower();
+                    Console.WriteLine(input);
+                }
+                else if (command == "FindIndex")
+                {
+                    string charToFindIndexOf = commandArgs[1];
+                    int lastIndex = input.LastIndexOf(charToFindIndexOf);
+
+                    Console.WriteLine(lastIndex);
+                }
+                else if (command == "Remove")
+                {
+                    int startIndex = int.Parse(commandArgs[1]);
+                    int count = int.Parse(commandArgs[2]);
+
+                    input = input.Remove(startIndex, count);
+
+                    Console.WriteLine(input);
+                }
+
+                commandLine = Console.ReadLine();
+            }
+        }
+    }
+}
