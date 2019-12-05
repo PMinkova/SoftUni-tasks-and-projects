0cfb6c5cc23844a8d304f9532efe5c395a6db03f
petyaminkova <petyaminkova91@abv.bg>
2019-12-05 20:41:43 +0200

    exam preparation

---
+++ Programming Fundamentals C#/Final Exam Demo - 06 April 19/01_Dictionary/Dictionary.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Channels;
+
+namespace _01_Dictionary
+{
+    class Dictionary
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> words = new Dictionary<string, List<string>>();
+
+            string input = Console.ReadLine();
+
+            string[] wordsWithDefinitions = input.Split(" | ");
+
+            for (int i = 0; i < wordsWithDefinitions.Length; i++)
+            {
+                string[] tokens = wordsWithDefinitions[i].Split(": ");
+                string word = tokens[0];
+                string definition = tokens[1];
+
+                AddWordsAndDefinitions(words, word, definition);
+            }
+
+            while (true)
+            {
+                input = Console.ReadLine();
+
+                if (input == "End")
+                {
+                    break;
+                } 
+
+                if (input == "List")
+                {
+                    PrintWords(words);
+                    break;
+                }
+
+                string[] newWords = input.Split(" | ");
+
+                PrintWordsAndDefinitions(newWords, words);
+            }
+        }
+
+        private static void AddWordsAndDefinitions(Dictionary<string, List<string>> words, string word, string definition)
+        {
+            if (!words.ContainsKey(word))
+            {
+                words.Add(word, new List<string>());
+            }
+
+            words[word].Add(definition);
+        }
+
+        private static void PrintWords(Dictionary<string, List<string>> words)
+        {
+            foreach (var word in words.OrderBy(x => x.Key))
+            {
+                Console.Write(word.Key + " ");
+            }
+
+            Console.WriteLine();
+            return;
+        }
+
+        private static void PrintWordsAndDefinitions(string[] newWords, Dictionary<string, List<string>> words)
+        {
+            foreach (var word in newWords)
+            {
+                if (words.ContainsKey(word))
+                {
+                    Console.WriteLine(word);
+                    words[word].OrderByDescending(x => x.Length).ToList().ForEach(x => Console.WriteLine($"-{x}"));
+                }
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals C#/Final Exam Demo - 06 April 19/02_Deciphering/Deciphering.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02_Deciphering
+{
+    class Deciphering
+    {
+        static void Main(string[] args)
+        {
+            string encryptedString = Console.ReadLine();
+            string[] substrings = Console.ReadLine().Split();
+
+            Regex regex = new Regex(@"^[{}|#d-z]+$");
+
+            Match match = regex.Match(encryptedString);
+
+            if (match.Success)
+            {
+                char[] encryptedStringAsArray = encryptedString.ToCharArray();
+                string decodedString = string.Empty;
+
+                foreach (var ch in encryptedStringAsArray)
+                {
+                    int currentCharAsNumber = ch - 3;
+                    decodedString += (char)currentCharAsNumber;
+                }
+
+                string firstSubstring = substrings[0];
+                string secondSybstring = substrings[1];
+
+                if (decodedString.Contains(firstSubstring))
+                {
+                    decodedString = decodedString.Replace(firstSubstring, secondSybstring);
+                }
+
+                Console.WriteLine(decodedString);
+            }
+            else
+            {
+                Console.WriteLine("This is not the book you are looking for.");
+            }
+        }
+    }
+}
