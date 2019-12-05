6983b4c8910ce265271f62632761c9ccf5d8699b
petyaminkova <petyaminkova91@abv.bg>
2019-12-05 14:47:40 +0200

    preparation for the final exam

---
+++ Programming Fundamentals C#/Final Exam - 14 April 2019 Group 1/01_ArrivingInKathMandu/ArrivingInKathmandu.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _01_ArrivingInKathmandu
+{
+    class ArrivingInKathmandu
+    {
+        static void Main(string[] args)
+        {
+            Regex regex = new Regex(@"^(?<name>[!@#$?A-Za-z0-9]+)=(?<length>\d+)<<(?<geoHashCode>.+)$");
+
+            string line = Console.ReadLine();
+            
+            while (line != "Last note")
+            {
+                Match match = regex.Match(line);
+
+                if (match.Success)
+                {
+                    string nameOfMountain = match.Groups["name"].Value;
+                    int geohashcodeLength = int.Parse(match.Groups["length"].Value);
+                    string geohashcode = match.Groups["geoHashCode"].Value;
+
+                    bool fullMatch = geohashcodeLength == geohashcode.Length;
+
+                    if (fullMatch)
+                    {
+                        PrintNameOfMountainAndGeohashcode(nameOfMountain, geohashcode);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nothing found!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Nothing found!");
+                }
+
+                line = Console.ReadLine();
+            }
+        }
+
+        private static void PrintNameOfMountainAndGeohashcode(string nameOfMountain, string geohashcode)
+        {
+            char[] nameOfMountainAsArray = nameOfMountain.ToCharArray();
+            nameOfMountainAsArray = nameOfMountainAsArray.Where(x => char.IsLetterOrDigit(x) || x == ' ').ToArray();
+            nameOfMountain = new string(nameOfMountainAsArray);
+            Console.WriteLine($"Coordinates found! {nameOfMountain} -> {geohashcode}");
+        }
+    }
+}
---
+++ Programming Fundamentals C#/Final Exam - 14 April 2019 Group 1/02_OnTheWayToAnnapurna/OnTheWayToAnnapurna.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02_OnTheWayToAnnapurna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> stores = new Dictionary<string, List<string>>();
+
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                string[] commandArgs = input.Split("->");
+                string command = commandArgs[0];
+                string store = commandArgs[1];
+
+                switch (command)
+                {
+                    case "Add":
+                        AddStore(stores, store, commandArgs);
+                        break;
+                    case "Remove":
+                        RemoveStore(stores, store);
+                        break;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine("Stores list:");
+
+            foreach (var store in stores.OrderByDescending(x => x.Value.Count).ThenByDescending(x => x.Key))
+            {
+                List<string> items = store.Value;
+                Console.WriteLine(store.Key);
+                items.ForEach(x => Console.WriteLine($"<<{x}>>"));
+            }
+        }
+
+        private static void RemoveStore(Dictionary<string, List<string>> stores, string store)
+        {
+            if (stores.ContainsKey(store))
+            {
+                stores.Remove(store);
+            }
+        }
+
+        private static void AddStore(Dictionary<string, List<string>> stores, string store, string[] commandArgs)
+        {
+            string[] items = commandArgs[2].Split(",");
+
+            if (!stores.ContainsKey(store))
+            {
+                stores.Add(store, new List<string>());
+            }
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                string currentItem = items[i];
+                stores[store].Add(currentItem);
+            }
+        }
+    }
+}
