42f261e08b97ae99a95ee2de131f999b1a1bb974
petyaminkova <petyaminkova91@abv.bg>
2020-02-07 16:18:21 +0200

    Custom Doubly Linked List

---
+++ C# Advanced/13. Workshop - Custom Doubly Linked List/CustomDoublyLinkedList/DoublyLinkedList.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace CustomDoublyLinkedList
+{
+    public class DoublyLinkedList
+    {
+        private ListNode head;
+        private ListNode tail;
+        public int Count { get; set; }
+
+        public void AddFirst(int element)
+        {
+            if (this.Count == 0)
+            {
+                this.head = this.tail = new ListNode(element);
+            }
+            else
+            {
+                ListNode newHead = new ListNode(element);
+                ListNode oldHead = this.head;
+
+                this.head = newHead;
+                oldHead.PreviousNode = newHead;
+                newHead.NextNode = oldHead;
+            }
+
+            this.Count++;
+        }
+
+        public void AddLast(int element)
+        {
+            if (this.Count == 0)
+            {
+                this.head = this.tail = new ListNode(element);
+            }
+            else
+            {
+                ListNode newTail = new ListNode(element);
+                ListNode oldTail = this.tail;
+
+                this.tail = newTail;
+                newTail.PreviousNode = oldTail;
+                oldTail.NextNode = newTail;
+            }
+
+            this.Count++;
+        }
+
+        public int RemoveFirst()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("The list is empty");
+            }
+
+            int removedElement = this.head.Value;
+
+            if (this.Count == 1)
+            {
+                this.head = this.tail = null;
+            }
+            else
+            {
+                this.head = this.head.NextNode;
+                this.head.PreviousNode = null;
+            }
+
+            this.Count--;
+            return removedElement;
+        }
+
+        public int RemoveLast()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("The List is empty");
+            }
+
+            int removedElement = this.tail.Value;
+
+            if (this.Count == 1)
+            {
+                this.head = this.tail = null;
+            }
+            else
+            {
+                this.tail = this.tail.PreviousNode;
+                this.tail.NextNode = null;
+            }
+
+            this.Count--;
+            return removedElement;
+        }
+
+        public void ForEach(Action<int> action)
+        {
+            ListNode currentElement = this.head;
+
+            while (currentElement != null)
+            {
+                action(currentElement.Value);
+                currentElement = currentElement.NextNode;
+            }
+        }
+
+        public int[] ToArray()
+        {
+            int[] arr = new int[Count];
+
+            for (int i = 0; i < Count; i++)
+            {
+                arr[i] = this.head.Value;
+                this.head = this.head.NextNode;
+            }
+
+            return arr;
+        }
+    }
+}
---
+++ C# Advanced/13. Workshop - Custom Doubly Linked List/CustomDoublyLinkedList/ListNode.cs
@@ -0,0 +1,16 @@
+﻿namespace CustomDoublyLinkedList
+{
+    public class ListNode
+    {
+        public ListNode(int value)
+        {
+            this.Value = value;
+        }
+        public int Value { get; set; }
+
+        public ListNode NextNode { get; set; }  
+
+        public ListNode PreviousNode { get; set; }
+
+    }
+}
