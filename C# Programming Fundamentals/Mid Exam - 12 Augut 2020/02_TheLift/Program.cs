c6fa04007ea7d909aef665937bcba43372e55d27
petyaminkova <petyaminkova91@abv.bg>
2022-10-10 23:53:04 +0300

    C# Fundamentals

---
+++ C# Programming Fundamentals/Mid Exam - 12 Augut 2020/02_TheLift/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+
+namespace _02_TheLift
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int waitingPeople = int.Parse(Console.ReadLine());
+
+            int[] currentStateOfTheLift = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int cabinsCount = currentStateOfTheLift.Length;
+            int capacity = 4;
+            bool hasMoreEmptySpots = false;
+
+            for (int i = 0; i < cabinsCount; i++)
+            {
+                int currentCount = currentStateOfTheLift[i];
+
+                if (currentCount <= capacity)
+                {
+                    int emptySpots = capacity - currentCount;
+
+                    if (emptySpots > waitingPeople)
+                    {
+                        currentStateOfTheLift[i] += waitingPeople;
+                        waitingPeople = 0;
+                        hasMoreEmptySpots = true;
+                        break;
+                    }
+                    else
+                    {
+                        currentStateOfTheLift[i] += emptySpots;
+                        waitingPeople -= emptySpots;
+                    }
+                }
+            }
+
+            if (hasMoreEmptySpots)
+            {
+                Console.WriteLine("The lift has empty spots!");
+            }
+
+            if (waitingPeople > 0)
+            {
+                Console.WriteLine($"There isn't enough space! {waitingPeople} people in a queue!");
+            }
+
+            Console.WriteLine(String.Join(" ", currentStateOfTheLift));
+        }
+    }
+}
