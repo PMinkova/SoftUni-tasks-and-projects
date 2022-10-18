3f4b006e83275942934d7876937bac862c65358b
petyaminkova <petyaminkova91@abv.bg>
2022-10-18 23:07:14 +0300

    Added exam preparation - 09.08.2020

---
+++ C# Programming Fundamentals/Final Exam 09 August 2020/01_WorldTour/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+
+namespace _01_WorldTour
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string stops = Console.ReadLine();
+
+            string input = Console.ReadLine();
+
+            while (input != "Travel")
+            {
+                string[] commandArgs = input.Split(":");
+
+                string command = commandArgs[0];
+
+                if (command == "Add Stop")
+                {
+                    stops = AddStop(commandArgs, stops);
+                    Console.WriteLine(stops);
+                }
+                else if (command == "Remove Stop")
+                {
+                    stops = RemoveElements(commandArgs, stops);
+                    Console.WriteLine(stops);
+                }
+                else if (command == "Switch")
+                {
+                    string oldString = commandArgs[1];
+                    string newString = commandArgs[2];
+
+                    if (stops.Contains(oldString))
+                    {
+                        stops = stops.Replace(oldString, newString);
+                        Console.WriteLine(stops);
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Ready for world tour! Planned stops: {stops}");
+        }
+
+        private static string RemoveElements(string[] commandArgs, string stops)
+        {
+            int startIndex = int.Parse(commandArgs[1]);
+            int endIndex = int.Parse(commandArgs[2]);
+
+            if (IsIndexValid(startIndex, stops) && IsIndexValid(endIndex, stops))
+            {
+                int count = endIndex - startIndex + 1;
+                stops = stops.Remove(startIndex, count);
+            }
+
+            return stops;
+        }
+
+        static string AddStop(string[] commandArgs, string stops)
+        {
+            int index = int.Parse(commandArgs[1]);
+            string stringToAdd = commandArgs[2];
+
+            if (IsIndexValid(index, stops))
+            {
+                stops = stops.Insert(index, stringToAdd);
+            }
+
+            return stops;
+        }
+
+        static bool IsIndexValid(int index, string stops)
+        {
+            if (0 <= index && index < stops.Length)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ C# Programming Fundamentals/Final Exam 09 August 2020/02_DestinationMapper/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02_DestinationMapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> destinations = new List<string>();
+            string input = Console.ReadLine();
+
+            string pattern = @"(=|\/)(?<destinations>[A-Z][A-Za-z]{2,})\1";
+
+            MatchCollection matches = Regex.Matches(input, pattern); ;
+            
+
+            foreach (Match match in matches)
+            {
+                destinations.Add(match.Groups["destinations"].Value);
+            }
+
+            int travelPoints = 0;
+
+            foreach (var destination in destinations)
+            {
+                travelPoints += destination.Length;
+            }
+
+            Console.WriteLine($"Destinations: {String.Join(", ", destinations)}");
+            Console.WriteLine($"Travel Points: {travelPoints}");
+        }
+    }
+}
