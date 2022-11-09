23e695575870d983f543fb315cfd40781c507a44
petyaminkova <petyaminkova91@abv.bg>
2022-11-09 14:49:08 +0200

    Added Problem 06 - Animals

---
+++ C# OOP/04. Inheritance - Exercise/Animals/Animals/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            try
+            {
+                Engine engine = new Engine();
+                engine.Run();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
