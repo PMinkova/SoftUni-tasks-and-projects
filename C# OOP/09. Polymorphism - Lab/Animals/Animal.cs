8f4ed0364e56e0fcca0b7688892cc3662ef7727b
petyaminkova <petyaminkova91@abv.bg>
2022-11-16 16:02:10 +0200

    Added solutions for Lab - Polymorphism

---
+++ C# OOP/09. Polymorphism - Lab/Animals/Animal.cs
@@ -0,0 +1,20 @@
+﻿namespace Animals
+{
+    public abstract class Animal
+    {
+        protected Animal(string name, string favouriteFood)
+        {
+            this.Name = name;
+            this.FavouriteFood = favouriteFood;
+        }
+
+        public string Name { get; protected set; }
+
+        public string FavouriteFood { get; protected set; }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {this.Name} and my fovourite food is {this.FavouriteFood}";
+        }
+    }
+}
---
+++ C# OOP/09. Polymorphism - Lab/Shapes/Rectangle.cs
@@ -0,0 +1,28 @@
+﻿namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double height;
+        private double width;
+
+        public Rectangle(double height, double width)
+        {
+            this.height = height;
+            this.width = width;
+        }
+        public override double CalculatePerimeter()
+        {
+            return 2 * this.height + 2 * this.width;
+        }
+
+        public override double CalculateArea()
+        {
+            return this.height * this.width;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + this.GetType().Name;
+        }
+    }
+}
