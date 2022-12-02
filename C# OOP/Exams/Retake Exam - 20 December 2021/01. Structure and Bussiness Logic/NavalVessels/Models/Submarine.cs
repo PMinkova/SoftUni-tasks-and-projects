dc4e0f11cb176edd223df9c1aeb05eaecc198dff
petyaminkova <petyaminkova91@abv.bg>
2022-12-02 23:37:57 +0200

    Added Exam preparation solutions from 20.12.2021

---
+++ C# OOP/Exams/Retake Exam - 20 December 2021/01. Structure and Bussiness Logic/NavalVessels/Models/Submarine.cs
@@ -0,0 +1,53 @@
+﻿namespace NavalVessels.Models
+{
+    using System.Text;
+    using Contracts;
+
+    public class Submarine : Vessel, ISubmarine
+    {
+        private const double InitialArmorThickness = 200;
+        private const double MainWeaponCaliberChange = 40;
+        private const double SpeedChange = 4;
+
+        public Submarine(string name, double mainWeaponCaliber, double speed)
+            : base(name, mainWeaponCaliber, speed, InitialArmorThickness)
+        {
+        }
+
+        public override void RepairVessel()
+        {
+            this.ArmorThickness = InitialArmorThickness;
+        }
+
+        public bool SubmergeMode { get; private set; }
+
+        public void ToggleSubmergeMode()
+        {
+            if (!this.SubmergeMode)
+            {
+                this.MainWeaponCaliber += MainWeaponCaliberChange;
+                this.Speed -= SpeedChange;
+            }
+            else
+            {
+                this.MainWeaponCaliber -= MainWeaponCaliberChange;
+                this.Speed += SpeedChange;
+            }
+
+            this.SubmergeMode = !this.SubmergeMode;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            string submergeModeOutput = this.SubmergeMode ? "ON" : "OFF";
+
+            sb
+                .AppendLine(base.ToString())
+                .AppendLine($" *Submerge mode: {submergeModeOutput}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
