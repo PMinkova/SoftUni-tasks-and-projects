6cbca65f8c9e9e61c2530604413b07b1b2c10b44
petyaminkova <petyaminkova91@abv.bg>
2022-12-09 16:35:12 +0200

    Added Problem 02. Animal Farm and Problem Description

---
+++ C# OOP/06. Encapsulation - Exercise/AnimalFarm/AnimalFarm/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace AnimalFarm
+{
+    using System;
+
+    using Models;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            var age = int.Parse(Console.ReadLine());
+
+            try
+            {
+                var chicken = new Chicken(name, age);
+
+                Console.WriteLine(
+                    "Chicken {0} (age {1}) can produce {2} eggs per day.",
+                    chicken.Name,
+                    chicken.Age,
+                    chicken.ProductPerDay);
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+            }
+        }
+    }
+}
