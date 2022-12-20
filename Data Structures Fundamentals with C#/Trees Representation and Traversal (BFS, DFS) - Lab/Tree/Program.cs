7471dc9c5e3b25d5f74353668a8e512214a2c7de
petyaminkova <petyaminkova91@abv.bg>
2022-12-20 15:03:50 +0200

    Added Trees Representation and Traversal (BFS, DFS) - Lab Solution

---
+++ Data Structures Fundamentals with C#/Trees Representation and Traversal (BFS, DFS) - Lab/Tree/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Tree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            var tree = new Tree<int>(1,
+                new Tree<int>(2,
+                    new Tree<int>(4),
+                                  new Tree<int>(5),
+                                  new Tree<int>(6)),
+                              new Tree<int>(3,
+                                  new Tree<int>(7)));
+
+            tree.RemoveNode(1);
+        }
+    }
+}
---
+++ Data Structures Fundamentals with C#/Trees Representation and Traversal (BFS, DFS) - Lab/Tree/Tree.cs
@@ -0,0 +1,177 @@
+﻿namespace Tree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Tree<T> : IAbstractTree<T>
+    {
+        private List<Tree<T>> children;
+        private T value;
+        private Tree<T> parent;
+
+        public Tree(T value)
+        {
+            this.value = value;
+            this.children = new List<Tree<T>>();
+        }
+
+        public Tree(T value, params Tree<T>[] children)
+            : this(value)
+        {
+            foreach (var child in children)
+            {
+                child.parent = this;
+                this.children.Add(child);
+            }
+        }
+
+        public void AddChild(T parentKey, Tree<T> child)
+        {
+            var searchedNode = FindNodeWithBfs(parentKey);
+
+            if (searchedNode == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            searchedNode.children.Add(child);
+            child.parent = this;
+        }
+
+        private Tree<T> FindNodeWithBfs(T parentKey)
+        {
+            var queue = new Queue<Tree<T>>();
+
+            queue.Enqueue(this);
+
+            while (queue.Any())
+            {
+                var subtree = queue.Dequeue();
+
+                if (subtree.value.Equals(parentKey))
+                {
+                    return subtree;
+                }
+
+                foreach (var child in subtree.children)
+                {
+                    queue.Enqueue(child);
+                }
+            }
+
+            return null;
+        }
+
+        public IEnumerable<T> DfsWithRecursion()
+        {
+            var result = new List<T>();
+
+            Dfs(this, result);
+
+            return result;
+        }
+
+        private void Dfs(Tree<T> tree, List<T> result)
+        {
+            foreach (var child in tree.children)
+            {
+                Dfs(child, result);
+            }
+
+            result.Add(tree.value);
+        }
+
+        public IEnumerable<T> OrderBfs()
+        {
+            var queue = new Queue<Tree<T>>();
+            var result = new List<T>();
+
+            queue.Enqueue(this);
+
+            while (queue.Any())
+            {
+                var subTree = queue.Dequeue();
+
+                result.Add(subTree.value);
+
+                foreach (var child in subTree.children)
+                {
+                    queue.Enqueue(child);
+                }
+            }
+
+            return result;
+        }
+
+        public IEnumerable<T> OrderDfs()
+        {
+            var stack = new Stack<Tree<T>>();
+            var result = new Stack<T>();
+
+            stack.Push(this);
+
+            while (stack.Any())
+            {
+                var node = stack.Pop();
+
+                foreach (var child in node.children)
+                {
+                    stack.Push(child);
+                }
+
+                result.Push(node.value);
+            }
+
+            return result;
+        }
+
+        public void RemoveNode(T nodeKey)
+        {
+            var nodeToRemove = FindNodeWithBfs(nodeKey);
+
+            if (nodeToRemove == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            var parentNode = nodeToRemove.parent;
+
+            if (parentNode == null)
+            {
+                throw new ArgumentException();
+            }
+
+            parentNode.children.Remove(nodeToRemove);
+            nodeToRemove.parent = null;
+        }
+
+        public void Swap(T firstKey, T secondKey)
+        {
+            var firsNode = FindNodeWithBfs(firstKey);
+            var secondNode = FindNodeWithBfs(secondKey);
+
+            if (firsNode == null || secondNode == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            var firstNodeParent = firsNode.parent;
+            var secondNodeParent = secondNode.parent;
+
+            if (firstNodeParent == null || secondNodeParent == null)
+            {
+                throw new ArgumentException();
+            }
+
+            var firstNodeIndex = firstNodeParent.children.FindIndex(x => x.Equals(firsNode));
+            var secondNodeIndex = secondNodeParent.children.FindIndex(x => x.Equals(secondNode));
+
+            firstNodeParent.children[firstNodeIndex] = secondNode;
+            secondNodeParent.children[secondNodeIndex] = firsNode;
+
+            secondNode.parent = firstNodeParent;
+            firsNode.parent = secondNodeParent;
+        }
+    }
+}
