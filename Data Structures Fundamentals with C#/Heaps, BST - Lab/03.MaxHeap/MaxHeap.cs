0fcf5f512eafc9d0e8743d8f824bbf5bd3c64a1a
petyaminkova <petyaminkova91@abv.bg>
2022-12-26 23:51:24 +0200

    Added Heaps and BST - Lab Solution

---
+++ Data Structures Fundamentals with C#/Heaps, BST - Lab/03.MaxHeap/MaxHeap.cs
@@ -0,0 +1,38 @@
+﻿namespace _03.MaxHeap
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class MaxHeap<T> : IAbstractHeap<T> where T : IComparable<T>
+    {
+        private List<T> elements;
+
+        public MaxHeap()
+        {
+            this.elements = new List<T>();
+        }
+
+        public int Size => this.elements.Count;
+
+        public void Add(T element)
+        {
+            
+        }
+
+        public T ExtractMax()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Peek()
+        {
+            if (!this.elements.Any())
+            {
+                throw new InvalidOperationException();
+            }
+
+            return this.elements[0];
+        }
+    }
+}
---
+++ Data Structures Fundamentals with C#/Heaps, BST - Lab/Demo/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Demo
+{
+    using System;
+    using _02.BinarySearchTree;
+
+    class Program
+    {
+        static void Main()
+        {
+            var binarySearchTree = new BinarySearchTree<int>();
+
+            binarySearchTree.Insert(8);
+            binarySearchTree.Insert(4);
+            binarySearchTree.Insert(2);
+            binarySearchTree.Insert(6); 
+            binarySearchTree.Insert(7); 
+            binarySearchTree.Insert(5); 
+
+            var newTree = binarySearchTree.Search(6);
+            
+
+            binarySearchTree.EachInOrder(Console.WriteLine);
+            newTree.EachInOrder(Console.Write);
+        }
+    }
+}
