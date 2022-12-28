ef2e3de50e7de34a4d4b5a70db4f507e363c1ac5
Petya Minkova <18217597+PMinkova@users.noreply.github.com>
2022-12-28 10:18:00 +0200

    Update MaxHeap.cs
    Added Max Heap implementation

--- Data Structures Fundamentals with C#/Heaps, BST - Lab/03.MaxHeap/MaxHeap.cs
+++ Data Structures Fundamentals with C#/Heaps, BST - Lab/03.MaxHeap/MaxHeap.cs
@@ -1,4 +1,4 @@
-﻿namespace _03.MaxHeap
+namespace _03.MaxHeap
 {
     using System;
     using System.Collections.Generic;
@@ -17,12 +17,66 @@
 
         public void Add(T element)
         {
-            
+            this.elements.Add(element);
+            this.HeapifyUp(this.elements.Count - 1);
         }
 
         public T ExtractMax()
         {
-            throw new NotImplementedException();
+            if (!this.elements.Any())
+            {
+                throw new InvalidOperationException();
+            }
+
+            var biggestElement = this.elements[0];
+            this.Swap(0, this.elements.Count - 1);
+            this.elements.RemoveAt(this.elements.Count - 1);
+            this.HeapifyDown(0);
+
+            return biggestElement;
+        }
+
+        private void HeapifyDown(int index)
+        {
+            var greaterIndex = GetBiggerChildIndex(index);
+
+            while (IsIndexvalid(greaterIndex) && this.IsGreater(greaterIndex, index))
+            {
+                this.Swap(index, greaterIndex);
+                index = greaterIndex;
+                greaterIndex = GetBiggerChildIndex(index);
+            }
+        }
+
+        private int GetBiggerChildIndex(int index)
+        {
+            var firstChildIndex = index * 2 + 1;
+            var secondChildIndex = index * 2 + 2;
+
+            if (IsIndexvalid(firstChildIndex) && IsIndexvalid(secondChildIndex))
+            {
+                if (this.elements[firstChildIndex].CompareTo(this.elements[secondChildIndex]) < 0)
+                {
+                    return secondChildIndex;
+                }
+
+                return firstChildIndex;
+            }
+            else if (!IsIndexvalid(firstChildIndex) && !IsIndexvalid(secondChildIndex))
+            {
+                return -1;
+            }
+            else if (!IsIndexvalid(secondChildIndex))
+            {
+                return firstChildIndex;
+            }
+
+            return secondChildIndex;
+        }
+
+        private bool IsIndexvalid(int index)
+        {
+            return 0 <= index && index < this.elements.Count;
         }
 
         public T Peek()
@@ -34,5 +88,35 @@
 
             return this.elements[0];
         }
+
+        private bool IsGreater(int firstIndex, int secondIndex)
+        {
+            return elements[firstIndex].CompareTo(elements[secondIndex]) > 0;
+        }
+
+        private int GetParentIndex(int index)
+        {
+            return (index - 1) / 2;
+        }
+
+        private void HeapifyUp(int index)
+        {
+            var parentIndex = this.GetParentIndex(index);
+
+            while (index > 0 && IsGreater(index, parentIndex))
+            {
+                this.Swap(index, parentIndex);
+                index = parentIndex;
+                parentIndex = this.GetParentIndex(index);
+            }
+        }
+
+        private void Swap(int index, int parentIndex)
+        {
+            var temp = elements[index];
+            this.elements[index] = elements[parentIndex];
+            this.elements[parentIndex] = temp;
+        }
     }
 }
+
