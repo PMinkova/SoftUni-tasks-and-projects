c67996d14ffdfc365323b55713ba11cef5828707
petyaminkova <petyaminkova91@abv.bg>
2023-01-06 21:56:10 +0200

    Added Red-Black-Tree-Exercise

--- Data Structures Advanced with C#/B-Trees - 2-3 Trees and Red-Black - Exercise/Demo/Program.cs
+++ Data Structures Advanced with C#/B-Trees - 2-3 Trees and Red-Black - Exercise/Demo/Program.cs
@@ -7,6 +7,15 @@ namespace Demo
         static void Main()
         {
             var rbt = new RedBlackTree<int>();
+
+            rbt.Insert(12);
+            rbt.Insert(6);
+            rbt.Insert(7);
+            rbt.Insert(3);
+            rbt.Insert(1);
+            rbt.Insert(9);
+            rbt.Insert(16);
+            rbt.Insert(15);
         }
     }
 }
