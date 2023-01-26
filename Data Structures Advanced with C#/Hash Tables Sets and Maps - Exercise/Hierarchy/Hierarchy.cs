3a994fc6b3b29fda03e5f3d7f84bda156c2cd329
petyaminkova <petyaminkova91@abv.bg>
2023-01-26 16:51:15 +0200

    Added Problem 03. Hierarchy

--- Data Structures Advanced with C#/Hash Tables Sets and Maps - Exercise/Hierarchy/Hierarchy.cs
+++ Data Structures Advanced with C#/Hash Tables Sets and Maps - Exercise/Hierarchy/Hierarchy.cs
@@ -3,54 +3,141 @@
     using System;
     using System.Collections.Generic;
     using System.Collections;
+    using System.Linq;
 
     public class Hierarchy<T> : IHierarchy<T>
     {
-        public Hierarchy(T value)
+        private class Node
         {
-            throw new NotImplementedException();
+            public Node(T value)
+            {
+                this.Value = value;
+                this.Children = new List<Node>();
+            }
+            public T Value { get; set; }
+
+            public Node Parent { get; set; }
+
+            public List<Node> Children { get; set; }
         }
 
-        public int Count => throw new NotImplementedException();
+        private Dictionary<T, Node> nodes;
+
+        private Node root;
+
+        public Hierarchy(T value)
+        {
+            this.root = new Node(value);
+            this.nodes = new Dictionary<T, Node>();
+            nodes.Add(value, root);
+        }
+
+        public int Count => this.nodes.Count;
 
         public void Add(T element, T child)
         {
-            throw new NotImplementedException();
+            if (!this.Contains(element) || this.Contains(child))
+            {
+                throw new ArgumentException();
+            }
+
+            var newChild = new Node(child);
+            newChild.Parent = this.nodes[element];
+            this.nodes[element].Children.Add(newChild);
+            this.nodes.Add(child, newChild);
         }
 
         public bool Contains(T element)
         {
-            throw new NotImplementedException();
+            return this.nodes.ContainsKey(element);
         }
 
         public IEnumerable<T> GetChildren(T element)
         {
-            throw new NotImplementedException();
+            if (!this.Contains(element))
+            {
+                throw new ArgumentException();
+            }
+
+            return this.nodes[element].Children.Select(n => n.Value);
         }
 
         public IEnumerable<T> GetCommonElements(Hierarchy<T> other)
         {
-            throw new NotImplementedException();
-        }
+            var result = new List<T>();
 
-        public IEnumerator<T> GetEnumerator()
-        {
-            throw new NotImplementedException();
+            foreach (var element in this.nodes.Keys)
+            {
+                if (other.nodes.ContainsKey(element))
+                {
+                    result.Add(element);
+                }
+            }
+
+            return result;
         }
 
         public T GetParent(T element)
         {
-            throw new NotImplementedException();
+            if (!this.nodes.ContainsKey(element))
+            {
+                throw new ArgumentException();
+            }
+
+            if (this.nodes[element].Parent == null)
+            {
+                return default;
+            }
+
+            return this.nodes[element].Parent.Value;
         }
 
         public void Remove(T element)
         {
-            throw new NotImplementedException();
+            if (!this.Contains(element))
+            {
+                throw new ArgumentException();
+            }
+
+            if (this.nodes[element].Equals(root))
+            {
+                throw new InvalidOperationException();
+            }
+
+            var node = this.nodes[element];
+            var parent = node.Parent;
+            var children = node.Children;
+
+            foreach (var child in children)
+            {
+                parent.Children.Add(child);
+                child.Parent = parent;
+            }
+
+            parent.Children.Remove(node);
+            this.nodes.Remove(element);
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            var queue = new Queue<Node>();
+
+            queue.Enqueue(this.root);
+
+            while (queue.Any())
+            {
+                var currentNode = queue.Dequeue();
+
+                yield return currentNode.Value;
+
+                foreach (var child in currentNode.Children)
+                {
+                    queue.Enqueue(child);
+                }
+
+            }
         }
 
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
     }
 }
