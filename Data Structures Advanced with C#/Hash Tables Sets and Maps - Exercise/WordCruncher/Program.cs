613d63f09c1fa82352f1b4a876bfff432fd2e141
petyaminkova <petyaminkova91@abv.bg>
2023-01-26 14:35:03 +0200

    Added Problem 02. Word Cruncher

--- Data Structures Advanced with C#/Hash Tables Sets and Maps - Exercise/WordCruncher/Program.cs
+++ Data Structures Advanced with C#/Hash Tables Sets and Maps - Exercise/WordCruncher/Program.cs
@@ -8,21 +8,91 @@ namespace WordCruncher
     {
         static void Main()
         {
-            var wordParts = Console.ReadLine().Split(", ").ToArray();
-            var word = Console.ReadLine();
+            var syllables = Console.ReadLine().Split(", ");
+            var targetWord = Console.ReadLine();
 
-            for (int i = 0; i < wordParts.Length; i++)
+            var cruncher = new Cruncher(syllables, targetWord);
+
+            foreach (var path in cruncher.GetSyllablePaths())
             {
-                if (word.StartsWith(wordParts[i]))
-                {
-                    Console.Write(wordParts[i] + " ");
+                Console.WriteLine(path);
+            }
+        }
+    }
 
-                    var length = wordParts[i].Length;
-                    word = word.Remove(0, length);
-                }
-
+    class Cruncher
+    {
+        private class Node
+        {
+            public Node(string syllable, List<Node> nextSyllables)
+            {
+                this.Syllable = syllable;
+                this.NextSyllables = nextSyllables;
             }
 
+            public string Syllable { get; set; }
+            public List<Node> NextSyllables { get; set; }
+        }
+
+        private List<Node> syllableGroups;
+
+        public Cruncher(string[] syllables, string targetWord)
+        {
+            this.syllableGroups = this.GenerateSyllableGroups(syllables, targetWord);
+        }
+
+        private List<Node> GenerateSyllableGroups(string[] syllables, string targetWord)
+        {
+            if (string.IsNullOrEmpty(targetWord) || syllables.Length == 0)
+            {
+                return null;
+            }
+
+            var resultValues = new List<Node>();
+
+            for (int i = 0; i < syllables.Length; i++)
+            {
+                var syllable = syllables[i];
+
+                if (targetWord.StartsWith(syllable))
+                {
+                    var nextSyllables = this.GenerateSyllableGroups(
+                        syllables.Where((_, index) => index != i).ToArray(),
+                        targetWord.Substring(syllable.Length)
+                    );
+
+                    resultValues.Add(new Node(syllable, nextSyllables));
+                }
+            }
+
+            return resultValues;
+        }
+
+        public IEnumerable<string> GetSyllablePaths()
+        {
+            var allPaths = new List<List<string>>();
+
+            this.GeneratePaths(this.syllableGroups, new List<string>(), allPaths);
+
+            return new HashSet<string>(allPaths.Select(x => string.Join(" ", x)));
+        }
+
+        private void GeneratePaths(List<Node> syllableGroups, List<string> currentPath, List<List<string>> allPaths)
+        {
+            if (syllableGroups == null)
+            {
+                allPaths.Add(new List<string>(currentPath));
+                return;
+            }
+
+            foreach (var node in syllableGroups)
+            {
+                currentPath.Add(node.Syllable);
+
+                this.GeneratePaths(node.NextSyllables, currentPath, allPaths);
+
+                currentPath.RemoveAt(currentPath.Count - 1);
+            }
         }
     }
 }
