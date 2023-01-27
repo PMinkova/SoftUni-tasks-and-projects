0819ea92f4b4defab2d17e067ce138795df4237f
petyaminkova <petyaminkova91@abv.bg>
2023-01-27 11:02:56 +0200

    Added Problem 01. CollectionOfPeople

---
+++ Data Structures Advanced with C#/Data Structures Augmentation/PeopleCollection/PeopleCollection.cs
@@ -0,0 +1,45 @@
+﻿namespace CollectionOfPeople
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class PeopleCollection : IPeopleCollection
+    {
+        public int Count => throw new NotImplementedException();
+
+        public bool Add(string email, string name, int age, string town)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Person Find(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Person> FindPeople(string emailDomain)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Person> FindPeople(string name, string town)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Person> FindPeople(int startAge, int endAge)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Person> FindPeople(int startAge, int endAge, string town)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Data Structures Advanced with C#/Data Structures Augmentation/PeopleCollection/PeopleCollectionSlow.cs
@@ -0,0 +1,72 @@
+﻿namespace CollectionOfPeople
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Security.Cryptography.X509Certificates;
+
+    public class PeopleCollectionSlow : IPeopleCollection
+    {
+        private List<Person> people;
+
+        public PeopleCollectionSlow()
+        {
+            this.people = new List<Person>();
+        }
+
+        public int Count => this.people.Count;
+
+
+        public bool Add(string email, string name, int age, string town)
+        {
+            if (this.Find(email) != null)
+            {
+                return false;
+            }
+
+            this.people.Add(new Person(email, name, age, town));
+            return true;
+        }
+
+        public bool Delete(string email)
+        {
+            var person = this.Find(email);
+
+            return this.people.Remove(person);
+        }
+
+        public Person Find(string email)
+        {
+            return this.people.FirstOrDefault(p => p.Email == email);
+        }
+
+        public IEnumerable<Person> FindPeople(string emailDomain)
+        {
+            return this.people
+                .Where(p => p.Email.EndsWith($"@{emailDomain}"))
+                .OrderBy(p => p.Email);
+        }
+
+        public IEnumerable<Person> FindPeople(string name, string town)
+        {
+            return this.people
+                .Where(p => p.Name == name && p.Town == town)
+                .OrderBy(p => p.Email);
+        }
+
+        public IEnumerable<Person> FindPeople(int startAge, int endAge)
+        {
+            return this.people.Where(p => p.Age >= startAge && p.Age <= endAge)
+                .OrderBy(p => p.Age)
+                .ThenBy(p => p.Email);
+        }
+
+        public IEnumerable<Person> FindPeople(int startAge, int endAge, string town)
+        {
+            return this.FindPeople(startAge, endAge)
+                .Where(p => p.Town == town)
+                .OrderBy(p => p.Age)
+                .ThenBy(p => p.Email);
+        }
+    }
+}
